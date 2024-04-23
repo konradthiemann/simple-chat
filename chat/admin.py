09e9aee5e804f131a4316b19c4d8ea65178fc44f
Konrad <konrad.gruss@t-online.de>
2024-04-23 08:58:46 +0200

    send message to backend via POST request

--- chat/admin.py
+++ chat/admin.py
@@ -1,3 +1,11 @@
+from re import search
+from chat.models import Chat, Message
 from django.contrib import admin
 
+class MessageAdmin(admin.ModelAdmin):
+    fields = ('chat', 'text','created_at', 'author', 'receiver')
+    list_display = ('created_at', 'author', 'text', 'receiver')
+    search_fields = ('text',)
 # Register your models here.
+admin.site.register(Message, MessageAdmin)
+admin.site.register(Chat)
--- chat/views.py
+++ chat/views.py
@@ -1,5 +1,12 @@
+from chat.models import Chat, Message
 from django.shortcuts import render
 
 # Create your views here.
 def index(request):
+    print(request.method)
+    if request.method == 'POST':
+        print("Request message is POST")
+        print(request.POST['textmessage'])
+        myChat = Chat.objects.get(id=1)
+        Message.objects.create(text=request.POST['textmessage'], chat=myChat, author=request.user, receiver=request.user)
     return render(request, 'chat/index.html', {'name': 'Konrad'})
