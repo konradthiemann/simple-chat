133d56d5fb70d604219c79a7095636c2edbf01ad
Konrad <konrad.gruss@t-online.de>
2024-04-23 16:09:52 +0200

    display messages in html

--- chat/views.py
+++ chat/views.py
@@ -9,4 +9,5 @@ def index(request):
         print(request.POST['textmessage'])
         myChat = Chat.objects.get(id=1)
         Message.objects.create(text=request.POST['textmessage'], chat=myChat, author=request.user, receiver=request.user)
-    return render(request, 'chat/index.html', {'name': 'Konrad'})
+    chatMessages = Message.objects.filter(chat__id=1)
+    return render(request, 'chat/index.html', {'messages':chatMessages})
