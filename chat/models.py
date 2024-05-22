df7fcd918226444770b1cac1cc706aa1e4b4fa6e
Konrad Thiemann <konrad.thiemann@mdm.de>
2024-05-22 10:47:59 +0200

    make this project run on my mac, did some formatting

--- chat/models.py
+++ chat/models.py
@@ -1,13 +1,31 @@
-from django.db import models
+"""imports"""
 from datetime import date
+
 from django.conf import settings
+from django.db import models
+
+
 # Create your models here.
+# BUG: could be changed to namedtupel here, because it only stores data
+# BUG: If there is the option to add people to a chat, namedtupel isnt an option anymore
 class Chat(models.Model):
     created_at = models.DateField(default=date.today)
 
 class Message(models.Model):
     text = models.CharField(max_length=500)
     created_at = models.DateField(default=date.today)
-    chat = models.ForeignKey(Chat, on_delete=models.CASCADE, related_name='chat_message_set', default=None, blank=True, null=True)
-    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='author_message_set')
-    receiver = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='receiver_message_set')
+    chat = models.ForeignKey(Chat,
+                             on_delete=models.CASCADE,
+                             related_name='chat_message_set',
+                             default=None,
+                             blank=True,
+                             null=True
+                             )
+    author = models.ForeignKey(settings.AUTH_USER_MODEL,
+                               on_delete=models.CASCADE,
+                               related_name='author_message_set'
+                               )
+    receiver = models.ForeignKey(settings.AUTH_USER_MODEL,
+                                 on_delete=models.CASCADE,
+                                 related_name='receiver_message_set'
+                                 )
--- chat/views.py
+++ chat/views.py
@@ -1,36 +1,40 @@
-from os import walk
-from chat.models import Chat, Message
-from django.db import IntegrityError
-from django.shortcuts import render
-from django.http import  HttpResponseRedirect
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
-from django.http import JsonResponse
 from django.core import serializers
+from django.db import IntegrityError
+from django.http import HttpResponseRedirect, JsonResponse
+from django.shortcuts import render
+
+from chat.models import Chat, Message
+
+
 # Create your views here.
 @login_required(login_url='/login/')
 def index(request):
+    """create a new message and renders the side"""
     if request.method == 'POST':
-        myChat = Chat.objects.get(id=1)
-        new_message = Message.objects.create(text=request.POST['textmessage'], chat=myChat, author=request.user, receiver=request.user)
+        my_chat = Chat.objects.get(id=1)
+        new_message = Message.objects.create(text=request.POST['textmessage'], chat=my_chat, author=request.user, receiver=request.user)
         serialized_object = serializers.serialize('json', [ new_message, ])
         return JsonResponse(serialized_object[1:-1], safe=False)
-    chatMessages = Message.objects.filter(chat__id=1)
-    return render(request, 'chat/index.html', {'messages':chatMessages})
+    chat_messages = Message.objects.filter(chat__id=1)
+    return render(request, 'chat/index.html', {'messages':chat_messages})
 
 def login_view(request):
+    """handle login"""
     redirect = request.GET.get('next')
     if request.method == 'POST':
-            user = authenticate(username=request.POST.get('username'), password=request.POST.get('password'))
-            if user:
-                login(request, user)
-                return HttpResponseRedirect(request.POST.get('redirect'))
-            else:
-                return render(request, 'auth/login.html', {'wrongPassword': True, 'redirect': redirect})
+        user = authenticate(username=request.POST.get('username'),
+                            password=request.POST.get('password'))
+        if user:
+            login(request, user)
+            return HttpResponseRedirect(request.POST.get('redirect'))
+        return render(request, 'auth/login.html', {'wrongPassword': True, 'redirect': redirect})
     return render(request, 'auth/login.html', {'redirect': redirect})
 
 def registration_view(request):
+    """handle registration"""
     if request.method == 'POST':
         username = request.POST.get('username')
         email = request.POST.get('email')
